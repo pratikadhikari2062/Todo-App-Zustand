5f370e2af2a07a5d31bf1a18d461472eb1cea392
pratikadhikari2062 <adhikaripratik522@gmail.com>
2026-04-16 15:20:57 +0545

    logic in todo

--- src/component/Close.tsx
+++ src/component/Close.tsx
@@ -1,19 +1,13 @@
 import { useTodoStore } from "./Zustandinput";
 
 const Close: React.FC = () => {
- const { todos, clearAll } = useTodoStore();
+  const { todos, clearAll } = useTodoStore();
   return (
     <div>
       {" "}
-      You have {todos.length} pending task{" "}
-      {todos.length > 0 && (
-        <button
-          onClick={clearAll}
-        >
-          Clear All
-        </button>
-      )}
+      You have {todos.length} {todos.length > 1 ? "todos" : "todo"}{" "}
+      {todos.length > 0 && <button onClick={clearAll}>Clear All</button>}
     </div>
-  );  
+  );
 };
 export default Close;
